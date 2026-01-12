7233fb95336b85ec7c33085e39963d822e728631
jo4o-gust4vo <j.gustavo.devs@gmail.com>
2026-01-12 14:43:12 -0300

    add metodo para tratar comentarios para inserir no bd

---
+++ database/normalization_comments.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+class Normalization_comments:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def normalization_comment(cls,list_comment: list):
+        new_list = []
+        for comment in list_comment:
+            new_list.append(f'\ '+comment.replace('"',"'"))
+        
+        return new_list
