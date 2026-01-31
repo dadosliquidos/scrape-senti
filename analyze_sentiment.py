142cfd86e59132e7b321994bf247436cde93b60a
jo4o-gust4vo <j.gustavo.devs@gmail.com>
2026-01-31 15:11:42 -0300

    implementando funcionalidade da LMM

--- analyze_sentiment.py
+++ analyze_sentiment.py
@@ -1,20 +1,30 @@
 from database.database import DB
 from classes.process.pnl import Pnl
 from classes.process.analyze_sentiment import Analyzer
+from classes.Instagram.scrapy import Scrape
+from llmms.teste import Chat
 import pandas as pd
+from dotenv import load_dotenv
+import os
+
+
 
 con = DB.conexao()
-
+load_dotenv()
 
 df = Pnl.get_comment(con)
 
 
-comentarios = Pnl.apply_stop_word(df)
+#comentarios = Pnl.apply_stop_word(df)
+#comentarios = 
 
 
-
+comentarios = DB.read_table(os.getenv('TABLE_W_S'))
 
 for comentario in comentarios:
-    sentimento = Analyzer.analyze_sentiment(comentario)
-    DB.insert_comment_w_sentiment(comentario,sentimento)
-    
+    #sentimento = Analyzer.analyze_sentiment(comentario)
+    sentimento = Chat.classificator(comment=comentario[1])
+    DB.insert_comment_w_sentiment(comentario[1],sentimento)
+
+
+ 
